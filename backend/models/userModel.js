3f67eaa442fe890c0a99a2ae5746802b8540798b
eshashetty19 <eshapshetty@gmail.com>
2025-01-31 14:32:15 +0530

    Fixed bcrypt invalid ELF header issue

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -8,5 +9,7 @@ const userSchema = new mongoose.Schema({
 }, { minimize: false })
 
 const userModel = mongoose.models.user || mongoose.model('user',userSchema);
+
+
 
 export default userModel
